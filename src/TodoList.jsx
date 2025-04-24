7a358db9ddc338b23e2923c64b7c9cf1cd73b0f2
fufex3934 <fufawakgari174@gmail.com>
2025-04-24 15:53:16 +0300

    jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,8 +1,16 @@
+const person = {
+  name:'Gregorio y. Zara',
+  theme:{
+    backgroundColor:'black',
+    color:'pink'
+  }
+};
+
 export default function TodoList() {
   return (
-    <div className="container mx-auto mt-4">
-      <h1 className="font-extrabold text-2xl py-2">Hedy Lamarr's Todos</h1>
-      <img className="pb-3" src="https://i.imgur.com/yXOvdOSs.jpg" alt="Hedy Lamarr" />
+    <div style={person.theme} className="container mx-auto mt-4">
+      <h1 className="font-extrabold text-2xl py-2">{person.name}'s Todos</h1>
+      <img className="pb-3 rounded-full" src="https://i.imgur.com/7vQD0fPs.jpg" alt="Hedy Lamarr" />
       <ul className="list-disc">
         <li>Invent new traffic lights</li>
         <li>Reherse a movie scene</li>
