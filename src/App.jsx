1e5fdaa332e1ad28c5c1fed6cf9366a92e93717f
fufex3934 <fufawakgari174@gmail.com>
2025-04-24 15:28:43 +0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
-}
+import Gallery from "./Gallery";
+
 function App() {
   return (
     <>
-      <div className="container mx-auto">
-        <h1 className="font-bold text-2xl py-4">Amazing Scientists</h1>
-        <div className="  flex gap-2">
-          <Profile />
-          <Profile />
-          <Profile />
-        </div>
-      </div>
+     
+      <Gallery/>
     </>
   );
 }
---
+++ src/Gallery.jsx
@@ -0,0 +1,18 @@
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
+
+export default function Gallery() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-bold py-4 text-2xl">Amazing Scientists</h1>
+      <div className="flex space-x-3">
+      <Profile/>
+      <Profile/>
+      <Profile/>
+      </div>
+    </div>
+  )
+}
+
