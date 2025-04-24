1f951435fa096c460f6054a07d8911bd238dd88d
fufex3934 <fufawakgari174@gmail.com>
2025-04-24 16:19:45 +0300

    styling avatar

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -3,12 +3,15 @@ import { getImageUrl } from "./utils";
 export default function Avatar({ person, size }) {
   return (
     <div className="container mx-auto mt-4">
+      <div className="border border-gray-500 w-50 h-50 rounded-2xl flex items-center justify-center">
       <img
         src={getImageUrl(person)}
         alt={person.name}
         width={size}
         height={size}
+        className=" rounded-full  "
       />
+      </div>
     </div>
   );
 }
